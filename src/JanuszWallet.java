8109de9db64b2029a902ce9dc862b62db7f0940d
krzysiek <krzysztofziemski@wp.pl>
2020-03-12 15:00:28 +0100

    1st version wallet group app

---
+++ src/JanuszWallet.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class JanuszWallet extends Wallet{
+    
+    private double money;
+    public JanuszWallet() {
+        moneyType = MoneyType.PLN;
+        money = 200.0;
+    }
+    @Override
+    public double addMoney(double money) {
+        return this.money +=  money;
+    }
+    @Override
+    public double spendingMoney(double money) {
+        return this.money -= money;
+    }
+    @Override
+    public double amountOfMoney() {
+        return money;
+    }
+}
+
