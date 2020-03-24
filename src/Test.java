863a78c86e4de3c2393ff48ec499d04e1df2c3bd
sujkowskidawid <sujkowskidawid942gmail.com>
2020-03-24 12:52:02 +0100

    zmiany zmiany

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,20 @@
+import java.util.Scanner;
+
 public class Test {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         JanuszWallet januszWallet = new JanuszWallet();
-        System.out.println(januszWallet.amountOfMoney());
-        System.out.println(januszWallet.spendingMoney(50));
-        System.out.println(januszWallet.addMoney(120.5));
+        System.out.println("Podaj operację na portfelu: ");
+        double walletOperation = scanner.nextDouble();
+        if (walletOperation > 0){
+            System.out.println(januszWallet.addMoney(walletOperation));
+        }
+        else if ( walletOperation < 0){
+            System.out.println(januszWallet.spendingMoney(-1*walletOperation));
+        }
+//        System.out.println(januszWallet.amountOfMoney());
+//        System.out.println(januszWallet.spendingMoney(70));
+//        System.out.println(januszWallet.addMoney(133));
         System.out.println(januszWallet);
     }
 }
